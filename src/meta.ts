8239593bcc01d33625b4bfb33edc94438fa57592
bobvane <wenbo007@gmail.com>
2026-08-17 04:31:50 +0800

    发布 V2.0.0：规则库扩展版
    - meta.ts / package.json / package-lock.json 版本号升级 0.1.0 → 2.0.0
    - CHANGELOG.md 新增 v2.0.0 条目

--- src/meta.ts
+++ src/meta.ts
@@ -4,7 +4,7 @@
  */
 export const APP_META = {
   name: 'CF-Workers-SUB-Next',
-  version: '0.1.0',
+  version: '2.0.0',
   repo: 'https://github.com/bobvane/CF-Workers-SUB-Next',
   repoShort: 'bobvane/CF-Workers-SUB-Next',
   author: 'Bob Vane',
